9af1432fded0564ce5d2e8c3052b964871b7f758
Egor <meekgot@gmail.com>
2020-11-05 23:09:07 +0300

    Начал делать компонент кнопка

---
+++ src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+    <button class="button" :class="classes">
+
+    </button>
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+
+    export default Vue.extend({
+        name: 'Button',
+
+        props: {
+            variations: {
+                type: Array,
+                default: () => [],
+            },
+        },
+
+        computed: {
+            classes() {
+                return this.variations.map(variation => {
+                    return `icon--${variation}`;
+                });
+            },
+        },
+    });
+</script>
---
+++ src/gui/components/GuiButtons.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="gui__container container">
+        <h2 class="gui__title">Кнопки</h2>
+
+        <div class="gui__block">
+            <Button />
+        </div>
+        <!-- /.gui__block -->
+    </div>
+    <!-- /.gui__container container -->
+</template>
+
+<script lang="ts">
+    import Vue from 'vue';
+    import Button from '../../components/Button.vue';
+
+    export default Vue.extend({
+        name: 'GuiButtons',
+
+        components: {
+            Button
+        }
+    })
+</script>
+
+<style>
+
+</style>
